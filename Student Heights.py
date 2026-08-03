0d8a32f06686c538f2be75cfd5958537a73ca235
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-04 04:11:09 +0500

    Student Heights.py

---
+++ Student Heights.py
@@ -0,0 +1,6 @@
+import numpy as np
+heights = [150, 155, 160, 162, 165, 168, 170, 172, 175, 180]
+Tenth = np.percentile(heights , 10)
+Nintyth = np.percentile(heights , 90)
+print("\n the tenth :  " , Tenth)
+print("\n the ninty : " , Nintyth)
