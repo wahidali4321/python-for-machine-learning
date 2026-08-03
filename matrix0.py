ebc8dee2d85a97dfd7a968591eb7684444f620e7
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-03 15:59:56 +0500

    matrix

---
+++ matrix0.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+arr = np.array([
+    [5, 10, 15],
+    [20, 25, 30],
+    [35, 40, 45]
+])
+
+# Standard deviation of each column
+column_std = np.std(arr, axis=0)
+
+# Standard deviation of each row
+row_std = np.std(arr, axis=1)
+
+print("Array:")
+print(arr)
+
+print("\nStandard deviation of each column:")
+print(column_std)
+
+print("\nStandard deviation of each row:")
+print(row_std)
