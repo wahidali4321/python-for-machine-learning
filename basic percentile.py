b5124c666cc1c6ee57af0139a91b324f16f3b4db
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-04 04:07:41 +0500

    basic percentile

--- basic percentile.py
+++ basic percentile.py
@@ -1,8 +1,8 @@
 import numpy as np
 marks = [55, 60, 65, 70, 75, 80, 85, 90]
-TF = np.percentile(marks)
-ST = np.percentile(marks)
-TFS = np.percentile(marks)
+TF = np.percentile(marks , 25)
+ST = np.percentile(marks , 50)
+TFS = np.percentile(marks , 75)
 print("25% : " , TF)
 print("\n 50% : " , ST)
 print("\n 75% : " , TFS)
