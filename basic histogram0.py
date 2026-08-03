ac37223a025d915fee6846ad4e2de23cd4ad9467
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-04 04:57:26 +0500

    basic histogram

---
+++ basic histogram0.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+marks = [45, 50, 55, 60, 65, 70, 75, 80, 85, 90]
+
+counts, bins, patches = plt.hist(marks, bins=5)
+
+print("Histogram counts:", counts)
+print("Bin edges:", bins)
+
+plt.title("Histogram of Marks")
+plt.xlabel("Marks")
+plt.ylabel("Frequency")
+plt.show()
