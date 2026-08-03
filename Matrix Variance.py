0a395eab15f80d5ac746a720e8567914a406b338
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-03 17:12:59 +0500

    Matrix Variance

---
+++ Matrix Variance.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+arr = np.array([
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+])
+
+# Variance of each column
+column_var = np.var(arr, axis=0)
+
+# Variance of each row
+row_var = np.var(arr, axis=1)
+
+print("Array:")
+print(arr)
+
+print("\nVariance of each column:")
+print(column_var)
+
+print("\nVariance of each row:")
+print(row_var)
