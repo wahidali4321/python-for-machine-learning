153b147abdbbf1455afac1d4976e248400a8a349
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-04 04:14:26 +0500

    Daily temperature

---
+++ Daily Temperatures.py
@@ -0,0 +1,9 @@
+import numpy as np
+temperatures = [30, 32, 31, 35, 34, 36, 33, 37, 38, 39]
+
+Twenth = np.percentile(temperatures , 20)
+Fourthy = np.percentile(temperatures , 40)
+Eigthy = np.percentile(temperatures , 80)
+print("\n the 20th is : " , Twenth)
+print("\n the fourthy is : " , Fourthy)
+print("\n the eighth is : " , Eigthy)
