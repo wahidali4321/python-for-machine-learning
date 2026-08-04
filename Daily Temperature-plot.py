226436638f13f38c4fa91b251a9bae2972d9500a
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-05 02:20:58 +0500

    Daily Temperature

---
+++ Daily Temperature-plot.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+days = np.array([1, 2, 3, 4, 5, 6, 7])
+temperature = np.array([30, 31, 29, 32, 34, 33, 31])
+plt.plot(days , temperature)
+plt.show()
