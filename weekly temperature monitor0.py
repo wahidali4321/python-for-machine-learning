c61b0848016d1848f92b17f1402c448cdd8e2c4d
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-05 03:46:14 +0500

    weekly temperature monitor0.py

---
+++ weekly temperature monitor0.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+days = np.array(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"])
+temperature = np.array([31, 33, 30, 34, 35, 36, 32])
+
+plt.plot(
+    days,
+    temperature,
+    marker="o",
+    color="red",
+    linestyle="--",
+    linewidth=2,
+    markersize=8
+)
+
+plt.title("Weekly Temperature Monitor")
+plt.xlabel("Days")
+plt.ylabel("Temperature (°C)")
+plt.grid(True)
+
+plt.show()
