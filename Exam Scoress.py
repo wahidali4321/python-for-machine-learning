77c44e871f3a7662d385e3cd5821128a72f45a26
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-05 03:26:37 +0500

    Exam Scores

---
+++ Exam Scoress.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+students = np.array([1,2,3,4,5,6])
+scores = np.array([55, 68, 75, 82, 90, 95])
+plt.title("Exam Scores")
+plt.xlabel("student")
+plt.ylabel("scores")
+plt.plot(students , scores)
+plt.show()
