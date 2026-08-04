f48d666b5c7429cb268213796176c46b9a891ee2
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-05 03:39:08 +0500

    Student Performance Tracker

---
+++ Student Performance Tracker.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+exams = np.array([1, 2, 3, 4, 5])
+marks = np.array([65, 72, 80, 78, 90])
+plt.plot(exams , marks)
+plt.title("Student Performance")
+plt.xlabel("Exam Number")
+plt.ylabel("Marks")
+plt.show()
