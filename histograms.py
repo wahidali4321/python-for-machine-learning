d6ba84b474914376768fd1056910e531ef0287bf
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-04 15:32:55 +0500

    histogrm

---
+++ histograms.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+randnms = np.random.uniform(1, 100, 100)
+
+count, bins, patches = plt.hist(randnms, bins=6)
+
+print("Count:", count)
+print("Bin edges:", bins)
+
+plt.xlabel("Random Numbers")
+plt.ylabel("Frequency")
+plt.title("Histogram of Random Numbers")
+plt.show()
