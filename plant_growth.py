871dee6b34eb8b92101a1c6b4972a8f75bb5ce9a
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-05 02:38:09 +0500

    plant_growth

---
+++ plant_growth.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+days = np.array([1, 3, 5, 7, 9])
+heights = np.array([5, 8, 12, 15, 18])
+
+plt.plot(days, heights)
+
+plt.title("Plant Growth Over Time")
+plt.xlabel("Days")
+plt.ylabel("Height (cm)")
+
+plt.show()
