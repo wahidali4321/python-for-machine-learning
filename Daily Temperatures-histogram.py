9575c0c7b224d7e3bf75d6ac8704bb63f1256267
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-04 16:27:50 +0500

    Daily Temperatures-histogram.py

---
+++ Daily Temperatures-histogram.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+temperature = [28, 29, 30, 31, 32, 33, 34, 35,
+               31, 30, 29, 28, 32, 33, 34]
+count , bin , patches = plt.hist(temperature , bins= 5 , edgecolor="black" )
+print("Total count is : " , count)
+plt.title("Temperature")
+plt.xlabel("numbers")
+plt.ylabel("temperature")
+plt.show()
