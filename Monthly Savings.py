0594b45024361f9c7488add6a2beaf23cb9bb393
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-05 02:24:35 +0500

    Monthly Savings.py

---
+++ Monthly Savings.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+months = np.array([1, 2, 3, 4, 5, 6])
+savings = np.array([5000, 6500, 7000, 8000, 9500, 11000])
+plt.plot(months , savings)
+plt.show()
