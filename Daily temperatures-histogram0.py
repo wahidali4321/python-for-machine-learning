0f41ffcca6af2285324989ec567e85246291fbec
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-04 16:30:39 +0500

    Daily temperature-histogram0.py

---
+++ Daily temperatures-histogram0.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+temperature = [
+    28, 29, 30, 31, 32, 33, 34, 35,
+    31, 30, 29, 28, 32, 33, 34
+]
+
+count, bin_edges, patches = plt.hist(
+    temperature,
+    bins=5,
+    edgecolor="black"
+)
+
+print("Total count is:", count)
+
+plt.title("Temperature Distribution")
+plt.xlabel("Temperature (°C)")
+plt.ylabel("Frequency")
+
+plt.show()
