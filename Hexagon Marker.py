b3b1a47f130a387b4efed9305085d7d07486636c
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-06 02:20:19 +0500

    Hexagon Marker

---
+++ Hexagon Marker.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+time = [0, 1, 2, 3, 4]
+speed = [0, 20, 40, 60, 80]
+plt.plot(time , speed , marker = 'h' , ms = 12 , mfc = 'pink')
+plt.grid(True)
+plt.show()
