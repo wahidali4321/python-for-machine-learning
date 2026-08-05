a0d7f35983be627a79d1a12f5343c591014545d4
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-06 01:29:54 +0500

    Fitness program-mec0

---
+++ Fitness program-mec0.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+dataset = np.array([1,2,3,4,5,6,7,8])
+plt.plot(dataset , marker = 'o' ,ms = 12 ,  mfc = 'red' , mec = 'red' , color = 'blue')
+plt.title("dataset of numbers ")
+plt.xlabel("dataset")
+plt.grid(True)
+plt.show()
