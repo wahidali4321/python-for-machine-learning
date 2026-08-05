b11c00cc3089368335393f3caa7ccf2609f5bf9b
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-06 01:23:02 +0500

    Fitness Progress0

---
+++ Fitness Progress0.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+weeks = np.array([1, 2, 3, 4, 5])
+weight = np.array([78, 77, 76, 75, 74])
+
+plt.plot(
+    weeks,
+    weight,
+    marker='^',          # Triangle marker
+    mfc='cyan',          # Marker face color
+    mec='black',         # Marker edge color
+    markersize=10,
+    color='blue',        # Line color
+    linewidth=2
+)
+
+plt.title("Fitness Progress")
+plt.xlabel("Weeks")
+plt.ylabel("Weight (kg)")
+plt.grid(True)
+
+plt.show()
