cf5463d94236642a8b831890b9b78cd02172d474
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-06 02:27:14 +0500

    Challenge (Mixed Markers)

---
+++ Challenge (Mixed Markers).py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+dataset = np.array([1,2,3,4,5,6,7,8,9,10])
+plt.plot(dataset , marker = 'o' , ms = 14 , mfc = "black" , mec = "red" , linewidth = 3 , color = 'red')
+plt.title("Challenge (Mixed Markers)")
+plt.xlabel("numbers")
+plt.ylabel("dataset")
+plt.show()
