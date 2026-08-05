a628ace3a368c37027c434bcd3a49c01514beb1c
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-05 15:29:24 +0500

    Fitness Progress

---
+++ Fitness Progress.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+weeks = np.array([1, 2, 3, 4, 5, 6])
+pushups = np.array([15, 18, 20, 24, 27, 30])
+
+plt.plot(weeks , pushups , '^')
+plt.title("Fitness progress")
+plt.xlabel("weeks")
+plt.ylabel("pushups")
+plt.show()
