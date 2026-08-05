3ecab0ee1c5fcbc045032e0780c66b62fbb077de
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-06 01:11:32 +0500

    Company Sales

---
+++ Company Sales.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+months = ["Jan", "Feb", "Mar", "Apr", "May"]
+sales = [1500, 1800, 1700, 2100, 2300]
+plt.plot(months , sales , marker = '*' , mec = 'black' , mfc = "purple")
+plt.show()
