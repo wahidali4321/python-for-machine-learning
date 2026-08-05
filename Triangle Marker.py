5d177c015cf2546870d7c1ba65f7cfa4e5b751d4
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-06 02:05:58 +0500

    Triangle Marker

---
+++ Triangle Marker.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+hours = [1, 2, 3, 4, 5]
+study = [2, 3, 5, 4, 6]
+plt.plot(hours , study , marker = '^' , ms = 12 , color = 'red')
+plt.grid(True)
+plt.show()
