94c25705d3210d214757e27b8d436ab99d84673f
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-05 15:25:39 +0500

    Exam Scores-s.py

---
+++ Exam Scores-s.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+subjects = np.array(["Math", "Physics", "Chemistry", "English"])
+scores = np.array([88, 76, 91, 84])
+plt.plot(subjects , scores , 's')
+plt.title("Exam scores")
+plt.xlabel("subject")
+plt.ylabel("scores")
+plt.show()
