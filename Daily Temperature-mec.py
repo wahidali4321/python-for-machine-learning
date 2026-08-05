a54a2c17f34d61591031ce2a66d02a498d99c5f3
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-06 01:00:52 +0500

    Daily Temperature-mec.py

---
+++ Daily Temperature-mec.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+temperature = [31, 33, 30, 34, 32]
+plt.plot(days , temperature , marker = 'D' ,ms = 10 , mec = "orange" )
+plt.title("Daily Temperature")
+plt.xlabel("days")
+plt.ylabel("temperature")
+plt.show()
