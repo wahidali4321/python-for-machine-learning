67b9d414fd4df1496b26c3f23b77b98536934846
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-08 04:25:55 +0500

    Student Marks 📚

---
+++ Student Marks 📚.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Subjects = np.array(['Math', 'Physics', 'English', 'CS', 'Statistics'])
+marks = np.array([75, 82, 68, 90, 78])
+plt.plot(Subjects , marks , marker = 'o')
+plt.title("Student Marks 📚")
+plt.xlabel("Subjects")
+plt.ylabel("marks")
+plt.grid(True)
