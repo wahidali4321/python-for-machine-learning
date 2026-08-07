24fe1c2f19cac68a76ed2009483df0da5bf0448f
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-08 02:26:58 +0500

    linestyle

---
+++ weekly.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+days = np.array(["mon" , "tue" , "wed" , "thur" , "fri" , "sat" , "sun"])
+Temperatures = np.array([30, 31, 29, 33, 35, 34, 32])
+plt.plot(days , Temperatures , ls = '-')
+plt.show()
