46de3e9d8e1755e504c55aadab436de8e1fb0330
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-08 03:10:07 +0500

    Monthly Sales 💰

---
+++ Monthly Sales 💰.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Months = np.array(["jan" , "feb" , "mar" , "apr" , "may"])
+sales = np.array([100, 120, 115, 140, 160])
+plt.plot(Months , sales , ls = '--')
+plt.title("Monthly Sales 💰")
+plt.grid(True)
+plt.xlabel("Months")
+plt.ylabel("Sales")
+plt.show()
