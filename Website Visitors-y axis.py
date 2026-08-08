f3f1e776671845e5a489f5b4721dcdbd3cc5e194
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-08 17:08:04 +0500

    Website Visitors-y axis

---
+++ Website Visitors-y axis.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+visitors = [120, 150, 180, 160, 220, 300, 250]
+plt.plot(days , visitors)
+plt.title("Website Visitors")
+plt.xlabel("days")
+plt.ylabel("visitors")
+plt.grid(axis= 'x')
+plt.show()
