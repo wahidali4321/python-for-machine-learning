d4bd94aa5aed8972661eb54961a9ee2af42d1dc0
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-08 16:39:17 +0500

    Student Marks-grid.py

---
+++ Student Marks-grid.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+subjects = ["Math", "Physics", "English", "CS", "Statistics"]
+marks = [75, 82, 68, 90, 78]
+plt.plot(subjects , marks , marker = 'o')
+plt.grid(True)
+plt.title("Student Marks")
+plt.xlabel("subjects")
+plt.ylabel("marks")
+plt.show()
