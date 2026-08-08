85a25f6ebf85e6076f1bf9c2d71f77046fcb3ea7
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-08 16:33:39 +0500

    Weekly Temperature-grid.py

---
+++ Weekly Temperature-grid.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+temperature = [30, 32, 31, 35, 34, 36, 33]
+plt.plot(days , temperature )
+plt.title("Weekly Temperature")
+plt.xlabel("Days")
+plt.ylabel("Temperature")
+plt.show()
