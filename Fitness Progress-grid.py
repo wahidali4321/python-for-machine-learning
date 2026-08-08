d77e18546166ea3465ab1ef124710b51462116d6
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-08 16:48:30 +0500

    Fitness Progress-grid.py

---
+++ Fitness Progress-grid.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+steps = [5000, 6500, 7000, 6200, 8000, 9000, 7500]
+plt.plot(days , steps , marker = 'o')
+plt.grid(axis= 'y')
+plt.title("Fitness Progress")
+plt.show()
