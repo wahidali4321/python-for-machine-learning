17610760a0ba30290264c2d10f7c1b0c52540b83
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-09 03:33:54 +0500

    Two Temperature Plots

---
+++ Two Temperature Plots.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+days = np.array(["Monday" , "Tuesday" , "wednesday" , "Thursday"])
+temperature = np.array([31,32,33,34])
+plt.subplot(1,2,1)
+plt.plot(days , temperature)
+
+days = np.array(["Friday","Saturday" , "Sunday"])
+temperature = np.array([35,3 , 39])
+
+plt.subplot(1,2,2)
+plt.plot(days , temperature)
+
+plt.show()
