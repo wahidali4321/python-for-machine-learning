78ffbd257bbfd697c642ca53614b68fdab8d4509
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-09 17:03:50 +0500

    tudent Attendance vs Marks

---
+++ tudent Attendance vs Marks.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Attendance = np.array([60, 65, 70, 75, 80, 85, 90, 95, 100])
+Marks = np.array([45, 50, 55, 62, 68, 75, 82, 90, 95])
+plt.scatter(Attendance , Marks , s=120 , alpha= 0.5)
+plt.ylabel("Marks")
+plt.xlabel("Attendance")
+plt.title("student attendance vs marks")
+plt.grid(True)
+plt.show()
