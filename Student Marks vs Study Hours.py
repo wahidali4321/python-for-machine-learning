69443ec5e5525a5e00a3c558ca8735b74b57e0fc
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-09 05:08:14 +0500

    Student Marks vs Study Hours

---
+++ Student Marks vs Study Hours.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+study_hours = [1, 2, 3, 4, 5, 6, 7]
+marks = [45, 50, 55, 62, 70, 78, 85]
+plt.scatter(study_hours , marks)
+plt.title("Student Marks vs study Hours")
+plt.xlabel("Study_hours")
+plt.ylabel("Marks")
