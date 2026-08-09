f32cf633646ec09f89a0aa1ffcc96c64e02b73a8
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 02:15:05 +0500

    Compare Two Groups ⭐

---
+++ Compare Two Groups ⭐.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+study_hours_A = [1, 2, 3, 4, 5, 6]
+marks_A = [45, 52, 60, 68, 75, 82]
+plt.scatter(study_hours_A , marks_A , marker = 'o' , color = "red" , label = "study_hours_A")
+
+
+study_hours_B = [1, 2, 3, 4, 5, 6]
+marks_B = [50, 58, 65, 72, 80, 88]
+plt.scatter(study_hours_B , marks_B , marker = 'o' , color = "yellow" , label = "study_hours_B")
+plt.xlabel("Study hours")
+plt.ylabel("Marks")
+plt.grid()
+plt.legend()
+plt.show()
