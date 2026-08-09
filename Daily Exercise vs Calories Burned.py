b84892db7ab3e6f7fdd3950e049e55671f8b6ffc
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-09 16:37:44 +0500

    Daily Exercise vs Calories Burned

---
+++ Daily Exercise vs Calories Burned.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Exercise_minutes = np.array([10, 20, 30, 40, 50, 60, 70])
+calories = np.array([80, 150, 220, 300, 380, 450, 530])
+plt.scatter(Exercise_minutes , calories , marker = 'o' , s = 100 , color = "red")
+plt.grid(True)
+plt.show()
