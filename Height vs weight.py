f4f276ce357babafe2bfb5a172a4240fb2f31664
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-09 05:14:23 +0500

    height vs weight

---
+++ Height vs weight.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+height = [150, 155, 160, 165, 170, 175, 180]
+weight = [45, 50, 55, 60, 65, 72, 78]
+plt.scatter(height , weight , marker = 'o')
+plt.title('Height vs Weight')
+plt.xlabel("Height in cm")
+plt.ylabel("Weight in kg")
+plt.show()
