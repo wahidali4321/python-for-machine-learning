5cf69dd3f08ee485003163b06269f2afcc041d2c
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 02:05:17 +0500

    Study Hours Comparison

---
+++ Study Hours Comparison.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+study_hours = [1, 2, 3, 4, 5, 6, 7]
+marks = [45, 50, 55, 62, 70, 78, 85]
+plt.scatter(study_hours , marks , marker = 'o')
+plt.show()
