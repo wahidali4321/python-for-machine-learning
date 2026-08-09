c01d114f4b916f6549e1fcf21b7d6a116cc40fba
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 02:54:14 +0500

    City A vs City B — Temperature & Electricity Usage

---
+++ City A vs City B — Temperature & Electricity Usage.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+temperature_A = [20, 22, 25, 28, 30, 32]
+usage_A = [120, 135, 150, 175, 190, 210]
+plt.scatter(temperature_A , usage_A , marker= '*' , color = "Green" , label = "City A")
+
+temperature_B = [20, 22, 25, 28, 30, 32]
+usage_B = [100, 115, 140, 160, 180, 195]
+plt.scatter(temperature_B , usage_B , marker= '*' , color = "Blue" , label = "City  B")
+plt.grid(True)
+plt.xlabel("Temperature")
+plt.ylabel("Useage")
+plt.legend()
+plt.title("City A vs City B — Temperature & Electricity Usage")
+plt.show()
