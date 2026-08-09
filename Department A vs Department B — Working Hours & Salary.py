9650d7c7bcd6ef5d82214edf6880929256674589
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 04:16:54 +0500

    Department A vs Department B — Working Hours & Salary

---
+++ Department A vs Department B — Working Hours & Salary.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+hours_A = [30, 35, 40, 45, 50, 55]
+salary_A = [30, 35, 42, 50, 58, 65]
+plt.scatter(hours_A , salary_A , marker= 's' , label = "Department A")
+
+hours_B = [30, 35, 40, 45, 50, 55]
+salary_B = [32, 38, 45, 53, 62, 70]
+plt.scatter(hours_B , salary_B , marker= 's' , label = "Department B")
+plt.title("Department A vs Department B — Working Hours & Salary")
+plt.xlabel("hours")
+plt.ylabel("salaries")
+plt.grid()
+plt.legend()
+plt.show()
