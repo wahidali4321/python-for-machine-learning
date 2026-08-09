95370133ee6617281f4104a2302ca7b750bf8e19
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 03:49:06 +0500

    Product A vs Product B — Price & Sales

---
+++ Product A vs Product B — Price & Sales.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+price_A = [10, 20, 30, 40, 50, 60]
+sales_A = [95, 85, 75, 65, 55, 45]
+plt.scatter(price_A , sales_A , marker= '*' , label = "Product A")
+
+price_B = [10, 20, 30, 40, 50, 60]
+sales_B = [110, 100, 90, 78, 65, 52]
+plt.scatter(price_B , sales_B , marker= '*' , label = "Product B")
+plt.title("Product A vs Product B — Price & Sales")
+plt.xlabel("Sales")
+plt.ylabel("Price")
+plt.legend()
+plt.grid()
+plt.show()
