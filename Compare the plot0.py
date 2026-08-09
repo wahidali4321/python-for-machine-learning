3103f7b520d8039bc8b51931e5b67c631fd8e6cc
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-09 17:45:06 +0500

    Compare the plot0.py

---
+++ Compare the plot0.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.array([1,2,3,4,5,6])
+y = np.array([11,22,33,11,11,22])
+
+plt.scatter(x,y)
+
+x = np.array([7,8,9])
+y = np.array([32,33,34])
+
+plt.scatter(x,y)
+plt.show()
