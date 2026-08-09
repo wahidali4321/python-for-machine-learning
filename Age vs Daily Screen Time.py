28f8b6323c66fdfcfc0279eb90f91c0e825111ca
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-09 16:54:30 +0500

    changes

--- Age vs Daily Screen Time.py
+++ Age vs Daily Screen Time.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 Age = np.array([10, 12, 14, 16, 18, 20, 22, 25])
 Screen_time = np.array([5, 6, 5.5, 7, 8, 7.5, 6, 5])
 
-plt.scatter(Age, Screen_time, marker='o', s=100, color='blue', alpha=0.6)
+plt.scatter(Age, Screen_time, marker='o', s=100, color='blue', alpha=1)
 
 plt.title("Age vs Daily Screen Time")
 plt.xlabel("Age")
