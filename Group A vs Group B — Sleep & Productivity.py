6a73f5a307a7cb22c99a374965e71198ae5cd237
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 04:00:07 +0500

    Group A vs Group B — Sleep & Productivity

---
+++ Group A vs Group B — Sleep & Productivity.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+sleep_A = [5, 6, 7, 8, 9, 10]
+productivity_A = [50, 60, 68, 75, 82, 85]
+plt.scatter(sleep_A , productivity_A , marker= 's' , label = "Group A" , color = "black")
+
+sleep_B = [5, 6, 7, 8, 9, 10]
+productivity_B = [45, 55, 65, 72, 78, 82]
+plt.scatter(sleep_B , productivity_B , marker= 's' , label = "Group B" , color = "green")
+plt.title("Group A vs Group B — Sleep & Productivity")
+plt.xlabel("Sleep")
+plt.ylabel("productivity")
+plt.grid(True)
+plt.legend()
+plt.show()
