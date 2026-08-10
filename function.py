8c29ad6cef7554b7ccdfbf151840591b012c764c
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 22:39:18 +0500

    function

---
+++ function.py
@@ -0,0 +1,10 @@
+def Cgpa(user1 , user2 , user3):
+    return user1 , user2 , user3
+user1 = int(input("enter the first person cgpa : "))
+user2 = int(input("enter the second person cgpa : "))
+user3 = int(input("enter the third person cgpa : "))
+
+
+result = Cgpa(user1 , user2 , user3)
+print(result)
+
