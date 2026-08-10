8ded46e2677a0705be3409fa40c74a52d6f5f531
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 16:18:34 +0500

    Student Marks-bar

---
+++ Student Marks-bar.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Subjects = np.array(['Math', 'Physics', 'English', 'CS', 'Statistics'])
+Marks = np.array([120, 150, 180, 140, 200])
+plt.bar(Subjects , Marks)
+plt.title("Monthly Sales")
+plt.xlabel("Subjects")
+plt.ylabel("Marks")
+plt.show()
