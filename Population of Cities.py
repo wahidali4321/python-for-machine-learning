a75a6748d226535e3dc9faf77559ff4be0c6e0f4
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 16:57:18 +0500

    Population of Cities

---
+++ Population of Cities.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Cities = np.array(["peshawar" , "karachi" , "lahore" , "quetta" ])
+population = np.array([2.0, 13.0, 20.0, 1.2])
+plt.bar(Cities , population)
+plt.show()
