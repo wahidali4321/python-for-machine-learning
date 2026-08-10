387822922be03f61d95365e9c452d8246f9333ad
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 16:28:38 +0500

    Product Sales

---
+++ Product Sales.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Products = np.array(["Laptop" , "Mobile" , "Tablet"])
+Sales = np.array([120, 250, 180])
+plt.bar(Products , Sales)
+plt.title("Product Sales")
+plt.xlabel("Product")
+plt.ylabel("Sales")
+plt.show()
