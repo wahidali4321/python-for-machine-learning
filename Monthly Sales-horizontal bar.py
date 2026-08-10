e934c45fe354b3de0b3d5a3a523fb45e5fc0820f
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 22:25:03 +0500

    Monthly Sales-horizontal bar

---
+++ Monthly Sales-horizontal bar.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Months = np.array(["jan" , "feb" , "march" , "aprail" , "may" , "jun"])
+sales = np.array([120, 150, 100, 180, 200 , 200])
+plt.barh(Months , sales)
+plt.title("Monthly Sales")
+plt.show()
