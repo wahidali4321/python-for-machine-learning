732ed647d2d5189eeb27b6efc6ea206caf60b8b4
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 16:37:08 +0500

    Weekly Expenses

---
+++ Weekly Expenses.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Days = np.array(["Mon" , 'Tue' , 'wed' , 'thu' , 'fri' , 'sat' , 'sun'])
+Expenses = np.array([500, 700, 400, 800, 600, 1000, 900])
+plt.bar(Days , Expenses)
+plt.title('Weekly Expenses')
+plt.xlabel("Day")
+plt.ylabel("Expensese")
+plt.show()
