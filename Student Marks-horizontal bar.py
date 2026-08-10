f2cf3d7962e4da7e58aac9a68ed14fcceadef2e0
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-10 22:19:31 +0500

    Student Marks-horizontal bar

---
+++ Student Marks-horizontal bar.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Subjects = np.array(["Maths" , "physics" , "English" , "CS" , "Statistics"])
+marks = np.array([75, 82, 68, 90, 78])
+plt.barh(Subjects , marks)
+plt.title("Student Marks")
+plt.show()
