8a1e94dd3b96069bc8dff252ca3b58fa0feaf9d5
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-11 11:45:50 +0500

    Student Marks by Subject

---
+++ Student Marks by Subject.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+zpoints = np.array([3, 15])
+npoints = np.array([3, 20])
+
+plt.plot(xpoints, ypoints,zpoints, npoints , marker='s')
+plt.show()
