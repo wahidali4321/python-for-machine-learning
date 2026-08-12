56c3edfee0e8ca2b25154aa180e11d0a38bd1e41
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-12 16:55:49 +0500

    Favorite Programming Languages

---
+++ Favorite Programming Languages.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Python = 40
+JavaScript = 25
+C = 20
+Java = 15
+values = np.array([Python , JavaScript , C , Java])
+labels = np.array(["Python " , "Javascript" , "C" , "Java"])
+plt.pie(values , labels = labels , autopct="%1.1f%%")
+plt.title("Favorite Programming Languages")
+plt.show()
