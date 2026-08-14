c61c7f4b084e0a4912ab5640912c65b81aee3754
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-15 02:17:45 +0500

    Different Shapes

---
+++ Different Shapes.py
@@ -0,0 +1,14 @@
+import numpy as np
+oneD = np.array([1,2,3,4,5,6])
+TwoD = np.array([
+    [1,2,3],[4,5,6]
+])
+ThreeD = np.array([
+    [
+        [1,2,3],[4,5,6]
+    ],
+    [[6,7,8],[9,9,9]]
+])
+print(oneD.shape)
+print(TwoD.shape)
+print(ThreeD.shape)
