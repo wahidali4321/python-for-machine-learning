810a433afa0d56aae5d4fd4e6594ca7cd2e89905
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-15 02:56:21 +0500

    Reshape Challenge ⭐

---
+++ Reshape Challenge ⭐.py
@@ -0,0 +1,13 @@
+import numpy as np
+arr = np.array([
+    1, 2, 3, 4, 5, 6,
+    7, 8, 9, 10, 11, 12,
+    13, 14, 15, 16, 17, 18,
+    19, 20, 21, 22, 23, 24
+])
+re = arr.reshape(4,6)
+are = re.reshape(6,4)
+print(re)
+print(are)
+print(re.shape)
+print(are.shape)
